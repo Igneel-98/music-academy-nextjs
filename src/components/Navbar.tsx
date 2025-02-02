4cfbaccc6482ca9004bc7e19c047cfe97ca10fa8
Igneel-98 <ghostriderattmd@gmail.com>
2025-02-03 01:56:41 +0530

    bug fix - home dropdown resolve

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,8 +19,7 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link href={"/"}><MenuItem setActive={setActive} active={active} item="Home"> </MenuItem></Link>
-        
+        <Link href={"/"}><MenuItem setActive={setActive} active={active} item="Home"></MenuItem></Link>      
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
